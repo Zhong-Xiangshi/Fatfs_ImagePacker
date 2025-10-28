99d9805b5bde8439827d64f17a248e6c53e23ab8
ZhongXiangshi <1696774082@qq.com>
2025-10-28 17:07:57 +0800

    添加输入参数FAT32和exFAT

--- README.md
+++ README.md
@@ -1,7 +1,15 @@
 # FatFs Image Packer
-此.exe程序会新建一个指定大小的文件，并将文件格式化为FatFs文件系统，然后拷贝指定文件夹的内容到里面。
+一个简单的exe程序：新建一个指定大小的文件，并将文件格式化为FatFs文件系统，然后拷贝指定文件夹的内容到里面。
 ``` PowerShell
-./Fatfs_ImagePacker.exe [输出镜像名] [镜像大小，单位bytes] [要拷贝的文件夹]
-#例子
-./Fatfs_ImagePacker.exe fatfs.img 67108864 my_files
+Fatfs_ImagePacker.exe -h
+Usage: Fatfs_ImagePacker.exe [options] [output_image.img] [size_in_bytes] [source_folder]
+Options:
+  -h, --help        Show this help message.
+  -f <format>       Specify the filesystem format. Options are:
+                    'FAT', 'FAT32', 'EXFAT' (default: EXFAT).
+
+Arguments default to:
+  - output_image.img: fatfs.img
+  - size_in_bytes:    33554432
+  - source_folder:    assets_to_pack
 ```
--- main.c
+++ main.c
@@ -11,6 +11,25 @@ char *disk_image_path="fatfs.img";
 uint64_t disk_image_size=(32 * 1024 * 1024);
 /* 默认要打包的文件夹名 */
 char* source_folder = "assets_to_pack";
+/* 默认的文件系统格式 */
+BYTE fs_format_type = FM_EXFAT;
+
+/*
+=================================================================================
+ 打印使用说明
+=================================================================================
+*/
+void print_usage(const char* prog_name) {
+    printf("Usage: %s [options] [output_image.img] [size_in_bytes] [source_folder]\n", prog_name);
+    printf("Options:\n");
+    printf("  -h, --help        Show this help message.\n");
+    printf("  -f <format>       Specify the filesystem format. Options are:\n");
+    printf("                    'FAT', 'FAT32', 'EXFAT' (default: EXFAT).\n");
+    printf("\nArguments default to:\n");
+    printf("  - output_image.img: %s\n", disk_image_path);
+    printf("  - size_in_bytes:    %llu\n", (unsigned long long)disk_image_size);
+    printf("  - source_folder:    %s\n", source_folder);
+}
 
 
 /*
@@ -18,39 +37,70 @@ char* source_folder = "assets_to_pack";
  主函数
 =================================================================================
 */
-
 int main(int argc, char *argv[]) {
     FATFS fs;
     FRESULT res;
     BYTE work[FF_MAX_SS];
+    char* format_str = "EXFAT"; // 用于打印日志
 
     // --- 从命令行参数解析配置 ---
-    if (argc > 1 && (strcmp(argv[1], "-h") == 0 || strcmp(argv[1], "--help") == 0)) {
-        printf("Usage: %s [output_image.img] [size_in_bytes] [source_folder]\n", argv[0]);
-        printf("  - If no arguments are provided, default values will be used.\n");
-        return 0;
-    }
-    
-    if (argc > 4) {
-        fprintf(stderr, "Error: Too many arguments.\n");
-        fprintf(stderr, "Usage: %s [output_image.img] [size_in_bytes] [source_folder]\n", argv[0]);
-        return 1;
-    }
-
-    if (argc >= 2) {
-        disk_image_path = argv[1];
-    }
-    if (argc >= 3) {
-        char* endptr;
-        unsigned long long size_bytes = strtoull(argv[2], &endptr, 10);
-        if (*endptr != '\0' || argv[2][0] == '\0' || size_bytes == 0) {
-            fprintf(stderr, "Error: Invalid size '%s'. Please provide a positive integer for bytes.\n", argv[2]);
-            return 1;
+    int arg_index = 1;
+    while (arg_index < argc) {
+        if (strcmp(argv[arg_index], "-h") == 0 || strcmp(argv[arg_index], "--help") == 0) {
+            print_usage(argv[0]);
+            return 0;
         }
-        disk_image_size = size_bytes;
-    }
-    if (argc >= 4) {
-        source_folder = argv[3];
+        // 检查文件系统格式选项
+        else if (strcmp(argv[arg_index], "-f") == 0) {
+            if (arg_index + 1 < argc) {
+                arg_index++; // 移动到格式值
+                if (stricmp(argv[arg_index], "FAT") == 0) {
+                    fs_format_type = FM_FAT;
+                    format_str = "FAT";
+                } else if (stricmp(argv[arg_index], "FAT32") == 0) {
+                    fs_format_type = FM_FAT32;
+                    format_str = "FAT32";
+                } else if (stricmp(argv[arg_index], "EXFAT") == 0) {
+                    fs_format_type = FM_EXFAT;
+                    format_str = "EXFAT";
+                } else {
+                    fprintf(stderr, "Error: Invalid format type '%s'. Use 'FAT', 'FAT32', or 'EXFAT'.\n", argv[arg_index]);
+                    return 1;
+                }
+            } else {
+                fprintf(stderr, "Error: Missing value for -f option.\n");
+                print_usage(argv[0]);
+                return 1;
+            }
+        }
+        // 非选项参数按顺序解析
+        else {
+            // 第一个非选项参数是镜像路径
+            if (disk_image_path == NULL || strcmp(disk_image_path, "fatfs.img") == 0) {
+                 disk_image_path = argv[arg_index];
+            }
+            // 第二个是大小
+            else if (disk_image_size == (32 * 1024 * 1024)) {
+                char* endptr;
+                unsigned long long size_bytes = strtoull(argv[arg_index], &endptr, 10);
+                if (*endptr != '\0' || argv[arg_index][0] == '\0' || size_bytes == 0) {
+                    fprintf(stderr, "Error: Invalid size '%s'. Please provide a positive integer for bytes.\n", argv[arg_index]);
+                    return 1;
+                }
+                disk_image_size = size_bytes;
+            }
+            // 第三个是源文件夹
+            else if (source_folder == NULL || strcmp(source_folder, "assets_to_pack") == 0) {
+                source_folder = argv[arg_index];
+            }
+            // 过多的参数
+            else {
+                fprintf(stderr, "Error: Too many arguments.\n");
+                print_usage(argv[0]);
+                return 1;
+            }
+        }
+        arg_index++;
     }
 
     printf("----------------------------------------\n");
@@ -60,11 +110,14 @@ int main(int argc, char *argv[]) {
            (unsigned long long)disk_image_size, 
            (double)disk_image_size / (1024.0 * 1024.0));
     printf("  - Source Folder: %s\n", source_folder);
+    printf("  - FS Format:     %s\n", format_str);
     printf("----------------------------------------\n\n");
 
     // --- 准备工作：格式化和挂载 ---
-    printf("Formatting the disk image...\n");
-    res = f_mkfs("0:", NULL, work, sizeof(work));
+    printf("Formatting the disk image with %s...\n", format_str);
+    // 使用 MKFS_PARM 结构体来指定格式
+    MKFS_PARM opt = { .fmt = fs_format_type };
+    res = f_mkfs("0:", &opt, work, sizeof(work));
     if (res != FR_OK) {
         fprintf(stderr, "ERROR: f_mkfs failed. FRESULT: %d\n", res);
         return -1;
